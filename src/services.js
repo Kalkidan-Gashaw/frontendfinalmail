88559315e0053137f498cc68d477640af634c267
Kalkidan-Gashaw <162141690+Kalkidan-Gashaw@users.noreply.github.com>
2025-01-29 15:19:04 +0300

    Update services.js

--- src/services.js
+++ src/services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5174",
+  baseURL: "https://frontendfinalmail.vercel.app",
 });
 
 axiosInstance.interceptors.request.use(
